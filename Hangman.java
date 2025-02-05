da93e711456d5853ae43f4e2da14e8e5b9e41774
AmelieC28 <ameliec1@cpp.edu>
2025-02-05 00:48:15 -0800

    Initial commit

---
+++ Hangman.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Hangman {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String wordToGuess = "I love cats";
+        StringBuilder hiddenWord = new StringBuilder(wordToGuess.replaceAll("[^ ]", "_"));
+        int attempts = 6;
+        boolean wordGuessed = false;
+
+        System.out.println("Welcome to Hangman!");
+        System.out.println("The word to guess is: " + hiddenWord);
+        System.out.println();
+
+        while (attempts > 0 && !wordGuessed) {
+            System.out.print("Guess a letter or the whole word: ");
+            String input = scanner.nextLine().trim().toLowerCase();
+
+            if (input.length() > 1) {
+                if (input.equals(wordToGuess.toLowerCase())) {
+                    hiddenWord = new StringBuilder(wordToGuess);
+                    wordGuessed = true;
+                } else {
+                    attempts--;
+                    System.out.println("Incorrect guess! You have " + attempts + " attempts left.");
+                }
+            } else if (input.length() == 1) {
+                char guess = input.charAt(0);
+                boolean correctGuess = false;
+
+                for (int i = 0; i < wordToGuess.length(); i++) {
+                    if (Character.toLowerCase(wordToGuess.charAt(i)) == guess) {
+                        hiddenWord.setCharAt(i, wordToGuess.charAt(i));
+                        correctGuess = true;
+                    }
+                }
+
+                if (correctGuess) {
+                    System.out.println("Correct guess! The word so far: " + hiddenWord);
+                } else {
+                    attempts--;
+                    System.out.println("Incorrect guess! You have " + attempts + " attempts left.");
+                }
+            }
+
+            if (hiddenWord.toString().equals(wordToGuess)) {
+                wordGuessed = true;
+                System.out.println("Congratulations! You've guessed the word: " + wordToGuess);
+            }
+
+            System.out.println();
+        }
+
+        if (!wordGuessed) {
+            System.out.println("Sorry, you've run out of attempts. The correct word was: " + wordToGuess);
+        }
+
+        scanner.close();
+    }
+}
